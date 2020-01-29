b88a582669e14afeb5c4af279232ca13c2f707ba
Eli Moore <eli.moore@mymail.champlain.edu>
2020-01-29 16:45:35 -0500

    Display and input progress

--- GPRO Framework/source/GPRO-FW-Launcher/Game/GPRO-FW-TicTacToe.c
+++ GPRO Framework/source/GPRO-FW-Launcher/Game/GPRO-FW-TicTacToe.c
@@ -56,8 +56,46 @@ inline gs_tictactoe_index gs_tictactoe_reset(gs_tictactoe game)
 	return total;
 }
 
+void displayBoard(gs_tictactoe game)
+{
+	system("clear");
 
-//-----------------------------------------------------------------------------
+	printf("  1  2  3 \n");
+
+	gs_tictactoe_index xpos, ypos;
+	for (ypos = 0; ypos < GS_TICTACTOE_BOARD_HEIGHT; ++ypos)
+	{
+		printf("%i", ypos + 1);
+		for (xpos = 0; xpos < GS_TICTACTOE_BOARD_WIDTH; ++xpos)
+		{
+			if(gs_tictactoe_getSpaceState(game, xpos, ypos) == gs_tictactoe_space_x)
+			{ printf(" X "); }
+			if(gs_tictactoe_getSpaceState(game, xpos, ypos) == gs_tictactoe_space_o)
+			{ printf(" O "); }
+			if(gs_tictactoe_getSpaceState(game, xpos, ypos) == gs_tictactoe_space_open)
+			{ printf(" _ "); }
+		}
+		printf("\n");
+	}
+}
+
+void getInput()
+{
+	printf("Enter an x coordinate: ");
+	int xCoord;
+	scanf("%i",&xCoord);
+	printf("\nEnter a y coordinate: ");
+	int yCoord;
+	scanf("%i", &yCoord);
+
+	printf("%i", xCoord);
+	printf("%i", yCoord);
+
+
+}
+
+
+//----------------------------------------------------------------------------
 // DEFINITIONS
 
 int launchTicTacToe()
@@ -66,8 +104,15 @@ int launchTicTacToe()
 
 	gs_tictactoe_reset(game);
 
+	displayBoard(game);
+	getInput();
+	//gs_tictactoe_setSpaceState(game, gs_tictactoe_space_x, 1, 2);
+	//displayBoard(game);
+
 	return 0;
 }
+
+
 
 
 //-----------------------------------------------------------------------------
